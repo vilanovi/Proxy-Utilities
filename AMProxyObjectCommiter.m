df5ed98327ea7abc049fc24cfa7b56deff2b35d3
Joan Martin <vilanovi@gmail.com>
2013-03-07 17:03:21 +0700

    Create AMProxyObjectCommiter.m

---
+++ AMProxyObjectCommiter.m
@@ -0,0 +1,176 @@
+//
+//  AMProxyObjectCommiter.m
+//  Created by Joan Martin.
+//  Take a look to my repos at http://github.com/vilanovi
+//
+
+#import "AMProxyObjectCommiter.h"
+
+NSString * const AMProxyObjectCommiterNillyfiedValuesKey = @"AMProxyObjectCommiterNillyfiedValuesKey";
+NSString * const AMProxyObjectCommiterChangedValuesKey = @"AMProxyObjectCommiterChangedValuesKey";
+
+@implementation AMProxyObjectCommiter
+{
+    id _object;
+    NSMutableDictionary *_keyedValues;
+    NSMutableSet *_nilValues;
+}
+
+- (id)__initWithObject:(id)object
+{
+    if (self)
+    {
+        _object = object;
+        _keyedValues = [NSMutableDictionary dictionary];
+        _nilValues = [NSMutableSet set];
+    }
+    return self;
+}
+
+#pragma mark Properties
+
+- (id)__object
+{
+    return _object;
+}
+
+#pragma mark NSProxy Methods
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)sel
+{
+    return [_object methodSignatureForSelector:sel];
+}
+
+- (void)forwardInvocation:(NSInvocation *)invocation
+{    
+    if (invocation.methodSignature == [_object methodSignatureForSelector:@selector(setValue:forKey:)])
+    {
+        __unsafe_unretained id tempValue = nil;
+        NSString *key = nil;
+        [invocation getArgument:&tempValue atIndex:2];
+        [invocation getArgument:&key atIndex:3];
+        
+        id value = tempValue;
+        
+        if (key != nil)
+        {
+            id oldValue = [_object valueForKey:key];
+            if (![oldValue isEqual:value])
+            {
+                if (value != nil)
+                {
+                    [_keyedValues setValue:value forKey:key];
+                    [_nilValues removeObject:key];
+                }
+                else
+                {
+                    [_nilValues addObject:key];
+                }
+            }
+            else
+            {
+                [_keyedValues removeObjectForKey:key];
+                [_nilValues removeObject:key];
+            }
+            
+            [invocation setTarget:nil];
+            [invocation invoke];
+        }
+        else
+        {
+            [invocation setTarget:_object];
+            [invocation invoke];
+        }
+    }
+    else if (invocation.methodSignature == [_object methodSignatureForSelector:@selector(valueForKey:)])
+    {
+        NSString *key = nil;
+        [invocation getArgument:&key atIndex:2];
+        
+        
+        if ([_keyedValues valueForKey:key])
+        {
+            [invocation setTarget:_keyedValues];
+            [invocation invoke];
+        }
+        else
+        {
+            [invocation setTarget:_object];
+            [invocation invoke];
+        }
+    }
+    else
+    {
+        [invocation setTarget:_object];
+        [invocation invoke];
+    }
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector
+{    
+    BOOL responds = [super respondsToSelector:aSelector];
+    
+    if (!responds)
+        responds = [_object respondsToSelector:aSelector];
+    
+    return responds;
+}
+
+#pragma mark Public Methods
+
+- (void)__commitChanges
+{
+    NSDictionary *keyValues = [_keyedValues copy];
+    [_object setValuesForKeysWithDictionary:keyValues];
+    
+    for (NSString *key in _nilValues)
+        [_object setValue:nil forKey:key];
+    
+    [_keyedValues removeAllObjects];
+    [_nilValues removeAllObjects];
+}
+
+- (void)__commitChangesForKey:(NSString*)key
+{
+    id value = [_keyedValues valueForKey:key];
+    
+    if (value)
+        [_object setValue:value forKey:key];
+    else if ([_nilValues containsObject:key])
+        [_object setValue:nil forKey:key];
+    
+    [_keyedValues removeObjectForKey:key];
+    [_nilValues removeObject:key];
+}
+
+- (NSDictionary*)__changes
+{
+    return @{AMProxyObjectCommiterChangedValuesKey : [_keyedValues copy],
+             AMProxyObjectCommiterNillyfiedValuesKey : [_nilValues copy]};
+}
+
+- (BOOL)__didChangeForKey:(NSString*)key
+{
+    return [_keyedValues valueForKey:key] != nil || [_nilValues containsObject:key];
+}
+
+- (id)__editedValueForKey:(NSString*)key
+{
+    id value = [_keyedValues valueForKey:key];
+    
+    if (value)
+        return value;
+    
+    if ([_nilValues containsObject:key])
+        return [NSNull null];
+
+    return nil;
+}
+
+- (id)__originalValueForKey:(NSString*)key
+{
+    return [_object valueForKey:key];
+}
+
+@end
+
